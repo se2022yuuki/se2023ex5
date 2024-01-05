1be6729502fb96bd6be388b7b512f086ce64e834
yuuki <r202203400es@jindai.jp>
2024-01-05 15:09:02 +0900

    version 3

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
   char str[] = "Hello, World!";
-  printf("%s\n", str);
+  printf("String: %s\n", str);
+  printf("Length: %lu\n", strlen(str));
   return 0;
 }
